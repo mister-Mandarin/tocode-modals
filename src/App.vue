3034f20332a844156959cc834768c62b0da87173
Andrey <andr.poluehktov@yandex.ru>
2022-09-15 13:11:29 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='wrapper'>
+    <Header />
+    <div class='wrapper-content'>
+      <section>
+        <div class='container'>
+          <div class='view-sm isCenter'>
+            <router-view />
+          </div>
+        </div>
+      </section>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+export default {
+	// eslint-disable-next-line vue/no-reserved-component-names
+	components: {Header, Footer}
+};
+</script>
---
+++ src/pages/home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class = "md-body">
+<!--    <MainForm />-->
+<!--    <ListNotes />-->
+    <p>123</p>
+  </div>
+</template>
+
+<script>
+// import Form from '@/components/Notes/Form';
+// import List from '@/components/Notes/List';
+
+export default {
+	// components: {
+	// 	MainForm: Form,
+	// 	ListNotes: List
+	// },
+	beforeMount() {
+		// при создании страницы ДО ЛОГИКИ вызываем метод searchNotes,
+		// который вставляет болванку если массив записей пустой
+		// this.searchNotes();
+		this.$store.watch(() => this.$store.getters.getListNotes);
+		this.$store.dispatch('searchNotes');
+	},
+	// watch следит за свойствами, и когда оно меняется, то выполняет функцию.
+	// https://vuejs.org/guide/essentials/watchers.html
+	watch: {}
+};
+</script>
